8aaff607dd8a4a9372a826a1e00baf0bda8af1cd
Jilver <jilverrivera1@gmail.com>
2021-11-03 11:17:00 -0500

    style: app theme config

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,15 +1,15 @@
 export const light = {
     background: "#EFF1F5",
+    background_auxiliar: "#FFFFFF",
     text: "#1D1D1D",
     primary: "#A7C5EB",
     secondary: "#949CDF",
-    tertiary: "#a8b2d1",
 };
 
 export const dark = {
-    background: "#202024",
+    background: "#2C2D35",
+    background_auxiliar: "#222729",
     text: "#F3F2F4",
-    primary: "#A7C5EB",
+    primary: "#087EC4",
     secondary: "#5429CF",
-    tertiary: "#a8b2d1",
 };
