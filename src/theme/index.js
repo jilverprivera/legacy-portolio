550ce7fd399fbb9797052903b99d4dac66f3b33e
Jilver <jilverrivera1@gmail.com>
2021-11-28 16:23:31 -0500

    style/app theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,13 @@
-import { dark, light } from "./theme";
+const theme = {
+    light: "#FFFFFF",
+    white: "#F3F2F4",
+    white_auxiliar: "#F6F8FA",
 
-export { light, dark };
+    dark: "#0E1821",
+    gray: "#9E9E9E",
+    black: "#1D1D1D",
+    primary: "#0E1821",
+    secondary: "#9F5DFC",
+};
+
+export default theme;
