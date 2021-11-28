ecb26034e6c0905eb2f816e912b471f08ff7c8b3
Jilver <jilverrivera1@gmail.com>
2021-11-28 16:34:06 -0500

    feat/language implementation

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,34 +1,23 @@
 import { useContext } from "react";
 import { useTranslation } from "react-i18next";
-
-// <--- IMPORTANT --->
+import Layout from "../src/components/layout";
+import Banner from "../src/components/projects/initial";
 import { AppContext } from "../src/context/AppContext";
 import Seo from "../src/seo";
 
-// <--- COMPONENTS --->
-import Layout from "../src/components/layout";
-import Banner from "../src/components/banner";
-import About from "../src/components/about";
-import Portfolio from "../src/components/portfolio";
-import Contact from "../src/components/contact";
-
-const Home = () => {
+const Portfolio = () => {
     const {
         language: { currentLanguage },
     } = useContext(AppContext);
     const [t] = useTranslation("global");
-
     return (
         <>
             <Seo lang={currentLanguage} title={t("web-title")} />
             <Layout>
                 <Banner />
-                <About />
-                <Portfolio/>
-                <Contact/>
             </Layout>
         </>
     );
 };
 
-export default Home;
+export default Portfolio;
