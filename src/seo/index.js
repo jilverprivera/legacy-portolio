7c2637cd2446c331512bd78054870aab31421de6
Jilver <jilverrivera1@gmail.com>
2021-11-28 16:36:21 -0500

    feat/app seo

--- src/seo/index.js
+++ src/seo/index.js
@@ -1,3 +1,68 @@
-import Seo from "./seo";
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
-export { Seo };
+const Seo = ({ description, lang, meta, title }) => {
+    const metaDescription =
+        description ||
+        `Jilver Pacheco's personal website, a electronic engineer and frontend developer passionate about learning things every day.`;
+
+    return (
+        <Helmet
+            htmlAttributes={{
+                lang,
+            }}
+            title={title}
+            titleTemplate={title}
+            meta={[
+                {
+                    name: `description`,
+                    content: metaDescription,
+                },
+                {
+                    property: `og:title`,
+                    content: title,
+                },
+                {
+                    property: `og:description`,
+                    content: metaDescription,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name: `twitter:card`,
+                    content: `summary`,
+                },
+                {
+                    name: `twitter:creator`,
+                    content: `@jilverprivera`,
+                },
+                {
+                    name: `twitter:title`,
+                    content: title,
+                },
+                {
+                    name: `twitter:description`,
+                    content: metaDescription,
+                },
+            ].concat(meta)}
+        />
+    );
+};
+
+Seo.defaultProps = {
+    lang: `en`,
+    meta: [],
+    description: ``,
+};
+
+Seo.propTypes = {
+    description: PropTypes.string,
+    lang: PropTypes.string,
+    meta: PropTypes.arrayOf(PropTypes.object),
+    title: PropTypes.string.isRequired,
+};
+
+export default Seo;
