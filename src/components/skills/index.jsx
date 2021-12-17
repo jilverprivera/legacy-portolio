6a8e84fab63e43c2679d0f6f4f7443927e3edb09
Jilver <jilverrivera1@gmail.com>
2021-12-16 23:27:34 -0500

    feat/skills components

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -1,25 +1,69 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Title } from "../../styles/typography";
-import { SkillContainer, SkillGrid, Wrapper } from "./styles";
-import NavButtons from "./navButton"
-import Technologies from "./technologies"
+import { AnimatePresence } from "framer-motion";
+
+import { SkillData } from "../../data";
+
 import Location from "../location";
 
+import { Container } from "../../styles";
+import {
+  Icon,
+  SkillGrid,
+  TechnologiesContent,
+  TechnologiesGroup,
+  TechnologiesWrapper,
+  TechnologyBtn,
+  Wrapper,
+} from "./styles";
+
 const Skills = () => {
-  const [selected, setSelected] = useState(1);
   const [t] = useTranslation("about");
+  const [selected, setSelected] = useState(1);
+
   return (
-    <SkillContainer>
+    <Container id="skills">
       <Wrapper>
         <Location location={t("skills.section-title")} />
-        <Title>{t("skills.title")}</Title>
         <SkillGrid>
-          <NavButtons selected={selected} setSelected={setSelected} />
-          <Technologies selected={selected} />
+          <TechnologiesGroup>
+            {SkillData.map((data) => (
+              <TechnologyBtn
+                key={data.id}
+                onClick={() => setSelected(data.id)}
+                selected={selected === data.id}
+              >
+                {data.title}
+              </TechnologyBtn>
+            ))}
+          </TechnologiesGroup>
+          <TechnologiesWrapper>
+            <AnimatePresence exitBeforeEnter>
+              {SkillData.filter((el) => el.id === selected).map((data) => (
+                <TechnologiesContent
+                  key={data.id}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  {data.skills.map((skill, index) => (
+                    <Icon
+                      key={skill.name}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ duration: 0.5, delay: index * 0.2 }}
+                    >
+                      {skill.icon}
+                    </Icon>
+                  ))}
+                </TechnologiesContent>
+              ))}
+            </AnimatePresence>
+          </TechnologiesWrapper>{" "}
         </SkillGrid>
       </Wrapper>
-    </SkillContainer>
+    </Container>
   );
 };
 
--- src/components/skills/styles.js
+++ src/components/skills/styles.js
@@ -2,16 +2,8 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import { SIZES } from "../../constants";
 
-export const SkillContainer = styled.section`
+export const Wrapper = styled.section`
   width: 100%;
-//   height: 100vh;
-  background-color: ${({ theme }) => theme.white_auxiliar};
-//   padding: 8rem auto 4rem;
-  border: 1px solid ${({ theme }) => theme.white_auxiliar};
-  `;
-  export const Wrapper = styled.section`
-  width: 100%;
-//   border: 1px solid #000;
   max-width: 125rem;
   margin: 8rem auto;
   display: flex;
@@ -21,19 +13,31 @@ export const SkillContainer = styled.section`
 `;
 export const SkillGrid = styled.div`
   width: 100%;
-  margin-top: 5rem;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(50rem, 1fr));
   column-gap: 1rem;
   row-gap: 1rem;
+  @media (max-width: 768px) {
+    display: block;
+    width: 90%;
+    margin: 0rem auto;
+  }
 `;
 
 export const TechnologiesGroup = styled.div`
   display: flex;
-
+  width: 100%;
   align-items: flex-start;
   justify-content: center;
   flex-direction: column;
+  margin-left: 3rem;
+  @media (max-width: 768px) {
+    align-items: center;
+    justify-content: space-around;
+    border: 1px solid #000;
+    flex-direction: row;
+    margin: 0rem auto;
+  }
 `;
 
 export const TechnologyBtn = styled.button`
@@ -41,11 +45,16 @@ export const TechnologyBtn = styled.button`
   outline: none;
   background-color: transparent;
   margin: 1rem 0rem;
-  font-size: ${SIZES.large};
+  font-size: ${SIZES.medium};
   font-weight: 600;
-  color: ${({ selected }) => selected};
+  color: ${({ selected, theme }) => (selected ? theme.black : theme.gray)};
+  transition: 0.4s;
   &:hover {
     cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    text-align: center;
   }
 `;
 export const Icon = styled(motion.span)`
