dd1f916ad95adac9eca7429705b6f0e6e8f0938a
Jilver <jilverrivera1@gmail.com>
2021-12-16 23:23:39 -0500

    feat/principal page

--- pages/index.js
+++ pages/index.js
@@ -5,34 +5,25 @@ import { AppContext } from "../src/context/AppContext";
 import Seo from "../src/seo";
 // <--- COMPONENTS --->
 import Layout from "../src/components/layout";
-import Home from "../src/components/home/Home";
-import Introduction from "../src/components/introduction";
+import Home from "../src/components/home";
 import Skills from "../src/components/skills";
-import Featured from "../src/components/home/Featured";
-import Contact from "../src/components/contact";
-// <--- HOOKS --->
-import { useSkew } from "../src/hooks/useSkew";
 import EducationExperience from "../src/components/educationExperience";
+import Portfolio from "../src/components/projects";
+import Contact from "../src/components/contact";
 
 const HomePage = () => {
   const { language } = useContext(AppContext);
   const { currentLanguage } = language;
-
-  const [t] = useTranslation("home");
-  const { scrollContainer } = useSkew();
-
+  const [t] = useTranslation("global");
   return (
     <>
       <Seo lang={currentLanguage} title={t("web-title")} />
       <Layout>
-        <div ref={scrollContainer}>
-          <Home />
-          <Introduction />
-          <Skills />
-          <EducationExperience />
-          <Featured />
-          <Contact />
-        </div>
+        <Home />
+        <Skills />
+        <EducationExperience />
+        <Portfolio />
+        <Contact />
       </Layout>
     </>
   );
