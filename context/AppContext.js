f5c3aa6778e6669d3924cfafc8dd29838da69ff0
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-06 15:03:05 -0500

    remove SSR consult and Navbar consult

--- context/AppContext.js
+++ context/AppContext.js
@@ -1,40 +1,11 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
 import PropTypes from "prop-types";
-import { useWindow } from "../hooks";
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  // --- STATES ---
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const [selector, setSelector] = useState("Skills");
-
-  const { rendering } = useWindow();
-  const [navbar, setNavbar] = useState(false);
-
-  useEffect(() => {
-    rendering &&
-      (window.onscroll = () => {
-        if (window.pageYOffset >= 80) {
-          setNavbar(true);
-        } else {
-          setNavbar(false);
-        }
-      });
-  }, [rendering]);
-
   // --- APP STATE ---
-  const state = {
-    layout: {
-      navbar,
-      openDrawer,
-      setOpenDrawer,
-    },
-    openDrawer,
-    setOpenDrawer,
-
-    information: { selector, setSelector },
-  };
+  const state = {};
   return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
 };
 
