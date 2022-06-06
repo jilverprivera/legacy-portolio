3336df044115ac6be4dad923e4503fbde38541e3
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-06 14:56:54 -0500

    SEO

--- seo/index.js
+++ seo/index.js
@@ -1,22 +1,22 @@
 import Head from "next/head";
 
-const Seo = ({ metadata = {} }) => {
-  const SEO = {
+const SEO = ({ metadata = {} }) => {
+  const data = {
     title: metadata.title || "Blog | Jilver Pacheco",
     date: metadata.date || new Date().toISOString(),
     slug: metadata.slug || "",
   };
   return (
     <Head>
-      <title>{SEO.title}</title>
-      <meta property="og:title" content={SEO.title} />
+      <title>{data.title}</title>
+      <meta property="og:title" content={data.title} />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@jilverprivera" />
-      <meta name="twitter:title" content={SEO.title} />
-      <meta property="article:published_time" content={SEO.date} />
+      <meta name="twitter:title" content={data.title} />
+      <meta property="article:published_time" content={data.date} />
     </Head>
   );
 };
 
-export default Seo;
+export default SEO;
