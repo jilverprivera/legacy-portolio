10a4c5984f4c3f0f43fb94352f503e26364dfc94
Jilver Pacheco <jilverrivera1@gmail.com>
2022-06-06 14:54:37 -0500

    add networks & variants helpers

---
+++ helpers/variants.js
@@ -0,0 +1,12 @@
+export const variants = {
+  hidden: { opacity: 0, y: -20 },
+  show: {
+    opacity: 1,
+    opacity: 1,
+    y: 0,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  exit: { opacity: 0, y: -10 },
+};
